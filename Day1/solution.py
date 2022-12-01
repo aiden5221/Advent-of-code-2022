4135c5c124ad21056ccb1b4b22bfead70c25b156
Aiden Jolley-Ruhn <72046405+aiden5221@users.noreply.github.com>
2022-12-01 07:35:11 -0500

    Day1

--- Day1/solution.py
+++ Day1/solution.py
@@ -23,26 +23,21 @@ for i in range(len(inpArray)):
 print(inpArray)
 
 resArr = []
-# Sum each elf's calories using 2 pointers
 ptrA, ptrB = 0, 0
-# While ptrB < len(inpArray)
+
 while ptrB < len(inpArray):
-    # Check if inpArray[ptrB] == '' or ptrB == len(inpArray) - 1
+
     if inpArray[ptrB] == '':
-        # Sum elements from inpArray[ptrA:ptrB]
         curSum = sum(inpArray[ptrA:ptrB])
-        # Append current sum to resArr  
         resArr.append(curSum)
-        # Set ptrA = ptrB
         ptrA = ptrB + 1
+
     elif ptrB == len(inpArray) - 1:
-        # Sum elements from inpArray[ptrA:ptrB]
         curSum = sum(inpArray[ptrA:ptrB+1])
-        # Append current sum to resArr  
         resArr.append(curSum)
 
-    # Increment ptrB
     ptrB += 1
+
 # Part1 Solution
 # print(max(resArr))
 
